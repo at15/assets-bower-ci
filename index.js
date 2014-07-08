b6066dbc5dd028cdb0e6fc4a8f6a7cb57d2de1a1
at15 <at15@at15-Aspire-E1-471G.(none)>
2014-07-08 20:47:39 +0800

    Add parseLib
    it can now get the files... but we also need to solve the dependency....

--- index.js
+++ index.js
@@ -32,46 +32,73 @@ Mgr.prototype.setConfig = function (configPath) {
     }
 };
 
-Mgr.prototype.parseLib = function () {
-
+Mgr.prototype.parseLib = function (libName) {
+    if (typeof this._libs[libName] !== 'object') {
+        var libFiles = [];
+        //TODO: check if it has any dependency
+        if (typeof this._config.libs[libName] === 'object') {
+            // get all the files
+            var fileGlob = this._config.libs[libName].files;
+            if (typeof fileGlob !== 'object') {
+                fileGlob = [fileGlob];
+            }
+            fileGlob.forEach(function (pattern) {
+                var files = glob.sync(pattern, {});
+                files.forEach(function (p) {
+                    libFiles.push(path.resolve(p));
+                });
+            });
+        }
+        if (libFiles.length === 0) {
+            log.warn('Lib: ' + libName + ' is empty! ');
+        }
+        this._libs[libName] = libFiles;
+    }
+    return this._libs[libName];
 };
 
 Mgr.prototype.readBower = function (pkgName) {
-    // final files in absolute path
-    var libFiles = [];
-    var bowerJsonPath = 'bower_components/' + pkgName + '/bower.json';
-    var bowerJson = {};
-    try {
-        bowerJson = JSON.parse(fs.readFileSync(bowerJsonPath));
-    } catch (e) {
-        // need to try .bower.json
-        bowerJsonPath = 'bower_components/' + pkgName + '/.bower.json';
+    if (typeof this._libs[pkgName] !== 'object') {
+        // final files in absolute path
+        var libFiles = [];
+        var bowerJsonPath = 'bower_components/' + pkgName + '/bower.json';
+        var bowerJson = {};
         try {
             bowerJson = JSON.parse(fs.readFileSync(bowerJsonPath));
         } catch (e) {
-            log.error('Can\'t read bower.json! ' + bowerJsonPath);
+            // need to try .bower.json
+            bowerJsonPath = 'bower_components/' + pkgName + '/.bower.json';
+            try {
+                bowerJson = JSON.parse(fs.readFileSync(bowerJsonPath));
+            } catch (e) {
+                log.error('Can\'t read bower.json! ' + bowerJsonPath);
+            }
         }
-    }
 
-    var mainFilesGlob = bowerJson.main;
-    if (typeof mainFilesGlob !== 'object') {
-        mainFilesGlob = [mainFilesGlob];
-    }
+        var mainFilesGlob = bowerJson.main;
+        if (typeof mainFilesGlob !== 'object') {
+            mainFilesGlob = [mainFilesGlob];
+        }
 
-    // change the directory
-    var cwd = process.cwd();
-    process.chdir('bower_components/' + pkgName);
+        // change the directory
+        var cwd = process.cwd();
+        process.chdir('bower_components/' + pkgName);
 
-    mainFilesGlob.forEach(function (pattern) {
-        var files = glob.sync(pattern, {});
-        files.forEach(function (p) {
-            libFiles.push(path.resolve(p));
+        mainFilesGlob.forEach(function (pattern) {
+            var files = glob.sync(pattern, {});
+            files.forEach(function (p) {
+                libFiles.push(path.resolve(p));
+            });
         });
-    });
 
-    // go back to the old dir
-    process.chdir(cwd);
-    return libFiles;
+        // go back to the old dir
+        process.chdir(cwd);
+        this._libs[pkgName] = libFiles;
+
+        // TODO:check the dependencies
+
+    }
+    return this._libs[pkgName];
 };
 
 Mgr.prototype.parseGroup = function () {
