6918b0578d85c93cda0bdf18747f946ea67ccf24
at15 <at15@at15-Aspire-E1-471G.(none)>
2014-07-10 19:48:20 +0800

    Add test for glob

---
+++ test/file.js
@@ -0,0 +1,19 @@
+require('should');
+var file = require('../file');
+var path = require('path');
+
+describe('file.glob', function () {
+    it('return empty when find nothing', function () {
+        file.glob('*.lq').should.eql([]);
+    });
+    it('return sth when found sth', function () {
+        var t = [
+            path.resolve('test/glob/a.js'),
+            path.resolve('test/glob/t.js')
+        ];
+        file.glob('test/glob/*.js').should.eql(t);
+    });
+//    it('return -1 when can\'t find element', function () {
+//        arr.inArray(a, 10086).should.eql(-1);
+//    });
+});
