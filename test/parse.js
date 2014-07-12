4e30177c73c9c0cc4d97a396187f0b16f2031bd5
at15 <at15@outlook.com>
2014-07-12 15:32:05 +0800

    Add test for parse
    how to type all the files ....

--- test/parse.js
+++ test/parse.js
@@ -7,15 +7,46 @@ describe('parse.lib', function () {
     // read the config
     var config = fh.readJson('assets.json');
     var p1 = new Parser({
-        dstFolder: 'site'
+        dstFolder: 'site',
+        libConfigs: config.libs
     });
-    it('return empty when find nothing', function () {
-        var jq = p1.parseLib(config.libs.jquery);
+    it(' test jquery ', function () {
+        var jq = p1.parseLib('jquery');
         jq.should.eql([path.resolve('site/lib/jquery/dist/jquery.js')]);
     });
+    it(' test bootstrap ', function () {
+        var bs = p1.parseLib('bootstrap');
+        var bsFiles = [
+            'site/lib/bootstrap/less/bootstrap.less',
+            'site/lib/bootstrap/dist/css/bootstrap.css',
+            'site/lib/bootstrap/dist/js/bootstrap.js',
+            'site/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
+            'site/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
+            'site/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
+            'site/lib/bootstrap/dist/fonts/glyphicons-halflings-regular.woff'
+        ];
+        var realBsFiles = [];
+        bsFiles.forEach(function (b) {
+            realBsFiles.push(path.resolve(b));
+        });
+        bs.should.eql(realBsFiles);
+    });
     it('won\'t merge file twice', function () {
-        var date_time = p1.parseLib(config.libs['date-time']);
-        date_time.should.eql([]);
+        var date_time = p1.parseLib('date-time');
+        var dtFiles =
+            [
+                'site/assets/lib/date-time/bootstrap-datetimepicker.css',
+                'site/assets/lib/date-time/bootstrap-datetimepicker.js',
+                'site/assets/lib/date-time/moment.js'
+            ];
+        var realFiles = [];
+        dtFiles.forEach(function(f){
+           realFiles.push((path.resolve(f)));
+        });
+        date_time.should.eql(realFiles);
+    });
+    it('can get dependencies',function(){
+
     });
 //    it('return sth when found sth', function () {
 //        var t = [
