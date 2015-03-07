ee40861147be75f97346d636fa54488aa13b5f44
at15 <at15@outlook.com>
2015-03-07 23:42:26 +0800

    Add tests for env lib
    also fix the require in log lib. it's log.js's fault. not the test's

--- test/env.js
+++ test/env.js
@@ -1,7 +1,25 @@
 // test env.
-var env = require('../lib/env.js');
-describe('test env read',function(){
-    it('return null when env is not set',function(){
-       expect(env.get('not_exists')).to.equal(null);
+var chai = require('chai');
+var expect = chai.expect;
+
+var env = require('../lib/env');
+describe('test env read', function () {
+    it('return null when env is not set', function () {
+        expect(env.get('not_exists')).to.equal(null);
+    });
+    it('return the right env variable', function () {
+        process.env.foo = 'bar';
+        env.refresh();
+        expect(env.get('foo')).to.equal('bar');
+    });
+    it('return default value', function () {
+        expect(env.get('jimmy', 'young')).to.equal('young');
+    });
+    it('throw exception when force value', function () {
+        var name = 'donotexists2';
+        expect(
+            function () {
+                env.get(name, '', true);
+            }).to.throw('config item ' + name + ' not found')
     });
 });
