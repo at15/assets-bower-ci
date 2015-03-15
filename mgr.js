cae94e1da11e7edb0732a3ad626623a3e7187936
at15 <at15@outlook.com>
2015-03-15 13:21:59 +0800

    The min cache is working

--- mgr.js
+++ mgr.js
@@ -2,3 +2,27 @@
  * Created by at15 on 15-3-10.
  */
 // The new assets manager
+
+// the mgr is just a test now
+
+var config = require('./lib/config');
+config.loadConfigJson('assets.json');
+var parser = require('./lib/parser');
+var min = require('./lib/min');
+
+parser.init();
+min.init();
+
+var pages = config.getAllPages();
+for (var pageName in pages) {
+    if (!pages.hasOwnProperty(pageName)) {
+        continue;
+    }
+    console.log('====== Page:', pageName, ' ======');
+    if (config.pageNeedMin(pageName)) {
+        console.log('page need min');
+        min.page(pageName);
+    } else {
+        console.log(parser.getPage(pageName));
+    }
+}
