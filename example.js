ddf3c488943bfd5536a0533024f2b1d952e80c05
at15 <at15@outlook.com>
2015-03-15 18:08:38 +0800

    Make mgr.js the main
    also bump to 0.3.1

---
+++ example.js
@@ -0,0 +1,8 @@
+/**
+ * Created by at15 on 15-3-15.
+ */
+var mgr = require('./mgr');
+
+mgr.init('assets.json');
+mgr.run();
+mgr.toJson();
--- mgr.js
+++ mgr.js
@@ -2,85 +2,92 @@
  * Created by at15 on 15-3-10.
  */
 // The new assets manager
-
-// the mgr is just a test now
+var mgr = {};
 
 var config = require('./lib/config');
-config.loadConfigJson('assets.json');
-
 var fh = require('./lib/file-helper');
 var parser = require('./lib/parser');
 var min = require('./lib/min');
 var output = require('./lib/output');
 
-parser.init();
-min.init();
 
-var pages = config.getAllPages();
-for (var pageName in pages) {
-    if (!pages.hasOwnProperty(pageName)) {
-        continue;
-    }
-    var finalOutput = {
-        js: [],
-        css: []
-    };
-    console.log('====== Page:', pageName, ' ======');
-    if (config.pageNeedMin(pageName)) {
-        console.log('page need min');
-        min.page(pageName);
+mgr.init = function (jsonPath) {
+    config.loadConfigJson(jsonPath);
+    parser.init();
+    min.init();
+};
 
-        var minResults = [];
-        var outputResults = {};
+mgr.run = function () {
+    var pages = config.getAllPages();
+    for (var pageName in pages) {
+        if (!pages.hasOwnProperty(pageName)) {
+            continue;
+        }
+        var finalOutput = {
+            js: [],
+            css: []
+        };
+        console.log('====== Page:', pageName, ' ======');
+        if (config.pageNeedMin(pageName)) {
+            console.log('page need min');
+            min.page(pageName);
 
-        var pageConfig = config.getPage(pageName);
+            var minResults = [];
+            var outputResults = {};
 
-        // load the libs
-        if (typeof pageConfig.libs === 'object') {
-            pageConfig.libs.forEach(function (libName) {
-                minResults = min.lib(libName);
-                outputResults = output.minLib(minResults, libName);
+            var pageConfig = config.getPage(pageName);
+
+            // load the libs
+            if (typeof pageConfig.libs === 'object') {
+                pageConfig.libs.forEach(function (libName) {
+                    minResults = min.lib(libName);
+                    outputResults = output.minLib(minResults, libName);
+                    if (outputResults.js) {
+                        finalOutput.js.push(outputResults.js);
+                    }
+                    if (outputResults.css) {
+                        finalOutput.css.push(outputResults.css);
+                    }
+                });
+            }
+
+            // load the groups
+            if (typeof  pageConfig.groups === 'object') {
+                pageConfig.groups.forEach(function (groupName) {
+                    minResults = min.group(groupName);
+                    outputResults = output.minGroup(minResults, groupName);
+                    if (outputResults.js) {
+                        finalOutput.js.push(outputResults.js);
+                    }
+                    if (outputResults.css) {
+                        finalOutput.css.push(outputResults.css);
+                    }
+                });
+            }
+
+            // load the files
+            if (typeof  pageConfig.files === 'object') {
+                minResults = min.files(fh.glob(pageConfig.files));
+                outputResults = output.minPageFile(minResults, pageName);
                 if (outputResults.js) {
                     finalOutput.js.push(outputResults.js);
                 }
                 if (outputResults.css) {
                     finalOutput.css.push(outputResults.css);
                 }
-            });
-        }
-
-        // load the groups
-        if (typeof  pageConfig.groups === 'object') {
-            pageConfig.groups.forEach(function (groupName) {
-                minResults = min.group(groupName);
-                outputResults = output.minGroup(minResults, groupName);
-                if (outputResults.js) {
-                    finalOutput.js.push(outputResults.js);
-                }
-                if (outputResults.css) {
-                    finalOutput.css.push(outputResults.css);
-                }
-            });
-        }
-
-        // load the files
-        if (typeof  pageConfig.files === 'object') {
-            minResults = min.files(fh.glob(pageConfig.files));
-            outputResults = output.minPageFile(minResults, pageName);
-            if (outputResults.js) {
-                finalOutput.js.push(outputResults.js);
             }
-            if (outputResults.css) {
-                finalOutput.css.push(outputResults.css);
-            }
-        }
 
-    } else {
-        finalOutput.js = fh.split(parser.getPage(pageName), 'js');
-        finalOutput.css = fh.split(parser.getPage(pageName), 'css');
+        } else {
+            finalOutput.js = fh.split(parser.getPage(pageName), 'js');
+            finalOutput.css = fh.split(parser.getPage(pageName), 'css');
+        }
+        //console.log(finalOutput)
+        output.addPage(pageName, finalOutput);
     }
-    //console.log(finalOutput)
-    output.addPage(pageName, finalOutput);
-}
+};
 
-output.toJson();
+mgr.toJson = function () {
+    output.toJson();
+};
+
+module.exports = mgr;
